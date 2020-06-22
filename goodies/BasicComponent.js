786e7c965832380ae07e6abd8a83341972d2f392
Felix Tröbinger <felix.troebinger@gmail.com>
2020-06-22 10:40:18 +0200

    add goodies, update readme

---
+++ goodies/BasicComponent.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class BasicComponent extends Component {
+
+    render() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        );
+    }
+
+}
+
+export default Metronome;
+
